3a6b58af424abd8e3b70a559abab87f106f7a917
kishansuthar000 <12kishan97@gmail.com>
2024-10-30 05:44:29 +0000

    index changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
       <div
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
-         <h1>This is QA</h1>
+         <h1>This is QA-ENV</h1>
       </div>
     </>
   );
