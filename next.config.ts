befe834d173c5034619fbcc06cea9d9d6d0ac1f3
kishansuthar000 <12kishan97@gmail.com>
2024-11-01 03:33:13 +0000

    QA Env Update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  basePath: process.env.BASE_PATH || '',
 };
 
 export default nextConfig;
