1087a4bcd8e31db8d2d8e66338c30d1a74b9d337
kishan suthar <56464242+kishansuthar000@users.noreply.github.com>
2024-11-03 18:04:22 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  basePath: "/dev",
+  basePath: process.env.BASE_PATH || "",
 };
 
 export default nextConfig;
